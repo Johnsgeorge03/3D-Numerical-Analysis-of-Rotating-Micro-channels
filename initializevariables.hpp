c9067dcaf71e2036822eb5f05568a952ae7120b5
Johnsgeorge03 <johnsgeorge03@gmail.com>
2021-05-01 14:54:16 +0530

    This is where variables and fields are initialized

--- initializevariables.hpp
+++ initializevariables.hpp
@@ -1,6 +1,8 @@
-int NX = 25, NY = 25, NZ = 150;
-double lengthX = 0.003, lengthY = 0.003, lengthZ = 0.018;
-double beginX = 0.0, beginY = 0.0 , beginZ = 0.001;
+// initialiazing geometry parameters
+
+int NX = 30, NY = 30, NZ = 100;
+double lengthX = 0.02, lengthY = 0.02, lengthZ = 1.0;
+double beginX = 0.0, beginY = 0.0 , beginZ = 0.0;
 
 Mesh mymesh_ (beginX, beginY, beginZ, NX, NY, NZ, lengthX, lengthY, lengthZ);
 
@@ -13,28 +15,40 @@ NK = mymesh_.getNK();
 Solution sol;
 Fields fieldOper(NI, NJ, NK);
 
+// variables at the start of 
 Fields::vec3dField U (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 Fields::vec3dField V (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 Fields::vec3dField W (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
-Fields::vec3dField U_ (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
-Fields::vec3dField V_ (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
-Fields::vec3dField W_ (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
-Fields::vec3dField UC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK))); // cell centre velocity correction
+Fields::vec3dField P (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+
+// variables at the start of timestep or previous timestep
+Fields::vec3dField UT (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+Fields::vec3dField VT (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK))); 
+Fields::vec3dField WT (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+Fields::vec3dField PT (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+
+// cell centre velocity correction
+Fields::vec3dField UC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 Fields::vec3dField VC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 Fields::vec3dField WC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 
+// pressure correction
+Fields::vec3dField PC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 
-Fields::vec3dField P (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
-Fields::vec3dField UW (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField(NK)));  //U wall 
-Fields::vec3dField UWC (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField(NK))); // U wall corrected
+// wall velocity at the start of timestep or previous timestep
+Fields::vec3dField UWT (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+Fields::vec3dField VWT (NI, Fields::vec2dField(NJ - 1, Fields::vec1dField(NK)));
+Fields::vec3dField WWT (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK - 1)));
+
+// face velocity and its correctios C stands for correction W for Wall
+Fields::vec3dField UW (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+Fields::vec3dField UWC (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 Fields::vec3dField VW (NI, Fields::vec2dField(NJ - 1, Fields::vec1dField(NK)));
 Fields::vec3dField VWC (NI, Fields::vec2dField(NJ - 1, Fields::vec1dField(NK)));
 Fields::vec3dField WW (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK - 1)));
 Fields::vec3dField WWC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK - 1)));
-Fields::vec3dField PC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));  //pressure correction
-Fields::vec3dField DPX (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
-Fields::vec3dField DPY (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK))); //pressure gradient in y direction
-Fields::vec3dField DPZ (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
+
+// massflux and its correction 
 Fields::vec3dField massFluxE (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField(NK)));
 Fields::vec3dField massFluxN (NI, Fields::vec2dField(NJ - 1, Fields::vec1dField(NK)));
 Fields::vec3dField massFluxT (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK - 1)));
@@ -42,187 +56,177 @@ Fields::vec3dField massFluxEC (NI - 1, Fields::vec2dField(NJ, Fields::vec1dField
 Fields::vec3dField massFluxNC (NI, Fields::vec2dField(NJ - 1, Fields::vec1dField(NK)));
 Fields::vec3dField massFluxTC (NI, Fields::vec2dField(NJ, Fields::vec1dField(NK - 1)));
 
-
-fieldOper.getGridInfoPassed(U, mymesh_, sol);  // passing some of the values from the mesh class mymesh_
+// passing details about the mesh and solution parameter like density visc etc...
+fieldOper.getGridInfoPassed(U, mymesh_, sol);
 fieldOper.getGridInfoPassed(V, mymesh_, sol);
 fieldOper.getGridInfoPassed(W, mymesh_, sol);
 
+fieldOper.getGridInfoPassed(UT, mymesh_, sol); 
+fieldOper.getGridInfoPassed(VT, mymesh_, sol);
+fieldOper.getGridInfoPassed(WT, mymesh_, sol);
+fieldOper.getGridInfoPassed(PT, mymesh_, sol);
+
 fieldOper.getGridInfoPassed(P, mymesh_, sol);
 fieldOper.getGridInfoPassed(PC, mymesh_, sol);
-fieldOper.getGridInfoPassed(DPX, mymesh_, sol);
-fieldOper.getGridInfoPassed(DPY, mymesh_, sol);
-fieldOper.getGridInfoPassed(DPZ, mymesh_, sol);
 fieldOper.getGridInfoPassed(massFluxE, mymesh_, sol);
 fieldOper.getGridInfoPassed(massFluxN, mymesh_, sol);
 fieldOper.getGridInfoPassed(massFluxT, mymesh_, sol);
 
+// used in setting boundary conditions
+    string north  = "NORTH"; 
+    string south  = "SOUTH";
+    string east   = "EAST"; 
+    string west   = "WEST";
+    string top    = "TOP"; 
+    string bottom = "BOTTOM";
 
+// initial guess and inlet velocity
+    double inletwvel  = 0.5;
+    double u_guess    = 0.0;
+    double v_guess    = 0.0;
+    double w_guess    = 0.0;
+    double massfluxwinlet = sol.density*inletwvel*U[1][1][1].St; // change the inlet velocity here
+    double massfluxwguess = sol.density*w_guess*U[1][1][1].St;
+    double massfluxuguess = sol.density*u_guess*U[1][1][1].Se;
+    double massfluxvguess = sol.density*v_guess*U[1][1][1].Sn;
 
+    //-------------------------------------BOUNDARY CONDITIONS-------------------------------------------//
 
-    cout<<"***************************"<< endl;
-   // cout<<"X"<<endl;
-   // for( unsigned int i =0; i<mymesh_.X.size(); i++)
-   // {
-   //     cout<< mymesh_.X[i]<<" ";
-   // }
-   // cout<<"Y"<<endl;
-   // 
-   // for(unsigned int i = 0; i< mymesh_.Y.size(); i++)
-   // {
-   //     cout<< mymesh_.Y[i]<<" ";
-   // }
-   // cout<<"Z"<<endl;
-
-   // for( unsigned int i =0; i<mymesh_.Z.size(); i++)
-   // {
-   //     cout<< mymesh_.Z[i]<<" ";
-   // }
-   // cout<<"XC"<<endl;
-
-   // for( unsigned int i =0; i<mymesh_.XC.size(); i++)
-   // {
-   //     cout<< mymesh_.XC[i]<<" ";
-   // }
-   // cout<<"YC"<<endl;
-
-   // for( unsigned int i =0; i<mymesh_.YC.size(); i++)
-   // {
-   //     cout<< mymesh_.YC[i]<<" ";
-   // }
-   // cout<<"ZC"<<endl;
-   // for( unsigned int i =0; i<mymesh_.ZC.size(); i++)
-   // {
-   //     cout<< mymesh_.ZC[i]<<" ";
-   // }
-   // cout<<"FX"<<endl;
-   // for( unsigned int i =0; i<mymesh_.FX.size(); i++)
-   // {
-   //     cout<< mymesh_.FX[i]<<" ";
-   // }
-   // cout<<"FY"<<endl;
-   // for( unsigned int i =0; i<mymesh_.FY.size(); i++)
-   // {
-   //     cout<< mymesh_.FY[i]<<" ";
-   // }
-   // cout<<"FZ"<<endl;
-   // for( unsigned int i =0; i<mymesh_.FZ.size(); i++)
-   // {
-   //     cout<< mymesh_.FZ[i]<<" ";
-   // }
-   // cout<<"U"<<endl;
-    string north = "NORTH"; string south = "SOUTH";
-    string east = "EAST"; string west = "WEST";
-    string top = "TOP"; string bottom = "BOTTOM";
-   // fieldOper.boundaryCondition(U,north, 1.0);
-    //fieldOper.boundaryCondition(U,south, 2.0);
-   // fieldOper.boundaryCondition(U,east, 3.0);
-   // fieldOper.boundaryCondition(U,west, 4.0);
-   // fieldOper.boundaryCondition(U,top, 5.0);
-   // fieldOper.boundaryCondition(U,bottom, 6.0);
-   // fieldOper.print3dmat(U);
-    fieldOper.initializeInternalField(P, 0.0);
-    fieldOper.initializeInternalField(PC, 0.0);
-   // fieldOper.copyInternalField(P, PP);
-   // fieldOper.linearextrapolateCondition(P, mymesh_.FX, mymesh_.FY, mymesh_.FZ, north);
-   // fieldOper.linearextrapolateCondition(P, mymesh_.FX, mymesh_.FY, mymesh_.FZ, south);
-    //fieldOper.linearextrapolateCondition(P, mymesh_.FX, mymesh_.FY, mymesh_.FZ, east);
-    //fieldOper.linearextrapolateCondition(P, mymesh_.FX, mymesh_.FY, mymesh_.FZ, west);
-    //fieldOper.linearextrapolateCondition(P, mymesh_.FX, mymesh_.FY, mymesh_.FZ, top);
-    //fieldOper.linearextrapolateCondition(P, mymesh_.FX, mymesh_.FY, mymesh_.FZ, bottom);
-
-    //FiniteMatrix::finiteMat AE2(AE);
-    //forAll(AE)
-    //{
-	//AE[i][j][k].value = 2.0;
-	//AE2[i][j][k].value = 3.0;
-    //}
-    //const double two = 2.0;
-    //FiniteMatrix::finiteMat AE3(AE2&&AE);
-    
-    //finiteObj.print3dmat(AE3);
-                              
-    //fieldOper.print3dmat(P);
-    double bottomwallvelformassfluxinit = sol.density*0.01*U[1][1][1].St;
-
-    // Equations
-
-    // setting some boundary conditions
+    	//----------------- WALL ------------------//
 
 	fieldOper.boundaryCondition(U, north, 0.0);
 	fieldOper.boundaryCondition(U, south, 0.0);
 	fieldOper.boundaryCondition(U, east, 0.0);
 	fieldOper.boundaryCondition(U, west, 0.0);
-	fieldOper.boundaryCondition(U, bottom, 0.0);
 
 	fieldOper.boundaryCondition(V, north, 0.0);
 	fieldOper.boundaryCondition(V, south, 0.0);
 	fieldOper.boundaryCondition(V, east, 0.0);
 	fieldOper.boundaryCondition(V, west, 0.0);
-	fieldOper.boundaryCondition(V, bottom, 0.0);
 
 	fieldOper.boundaryCondition(W, north, 0.0);
 	fieldOper.boundaryCondition(W, south, 0.0);
 	fieldOper.boundaryCondition(W, east, 0.0);
 	fieldOper.boundaryCondition(W, west, 0.0);
-	fieldOper.boundaryCondition(W, bottom, 0.01);
 
+	fieldOper.boundaryCondition(UT, north, 0.0);
+	fieldOper.boundaryCondition(UT, south, 0.0);
+	fieldOper.boundaryCondition(UT, east, 0.0);
+	fieldOper.boundaryCondition(UT, west, 0.0);
+
+	fieldOper.boundaryCondition(VT, north, 0.0);
+	fieldOper.boundaryCondition(VT, south, 0.0);
+	fieldOper.boundaryCondition(VT, east, 0.0);
+	fieldOper.boundaryCondition(VT, west, 0.0);
+
+	fieldOper.boundaryCondition(WT, north, 0.0);
+	fieldOper.boundaryCondition(WT, south, 0.0);
+	fieldOper.boundaryCondition(WT, east, 0.0);
+	fieldOper.boundaryCondition(WT, west, 0.0);
 
 	fieldOper.boundaryCondition(UW, north, 0.0);
 	fieldOper.boundaryCondition(UW, south, 0.0);
 	fieldOper.boundaryCondition(UW, east, 0.0);
 	fieldOper.boundaryCondition(UW, west, 0.0);
-	fieldOper.boundaryCondition(UW, bottom, 0.0);
 
 	fieldOper.boundaryCondition(VW, north, 0.0);
 	fieldOper.boundaryCondition(VW, south, 0.0);
 	fieldOper.boundaryCondition(VW, east, 0.0);
 	fieldOper.boundaryCondition(VW, west, 0.0);
-	fieldOper.boundaryCondition(VW, bottom, 0.0);
 
 	fieldOper.boundaryCondition(WW, north, 0.0);
 	fieldOper.boundaryCondition(WW, south, 0.0);
 	fieldOper.boundaryCondition(WW, east, 0.0);
 	fieldOper.boundaryCondition(WW, west, 0.0);
-	fieldOper.boundaryCondition(WW, bottom, 0.01);
 
-	fieldOper.boundaryCondition(UWC, north, 0.0);
-	fieldOper.boundaryCondition(UWC, south, 0.0);
-	fieldOper.boundaryCondition(UWC, east, 0.0);
-	fieldOper.boundaryCondition(UWC, west, 0.0);
-	fieldOper.boundaryCondition(UWC, bottom, 0.0);
+	fieldOper.boundaryCondition(UWT, north, 0.0);
+	fieldOper.boundaryCondition(UWT, south, 0.0);
+	fieldOper.boundaryCondition(UWT, east, 0.0);
+	fieldOper.boundaryCondition(UWT, west, 0.0);
 
-	fieldOper.boundaryCondition(VWC, north, 0.0);
-	fieldOper.boundaryCondition(VWC, south, 0.0);
-	fieldOper.boundaryCondition(VWC, east, 0.0);
-	fieldOper.boundaryCondition(VWC, west, 0.0);
-	fieldOper.boundaryCondition(VWC, bottom, 0.0);
+	fieldOper.boundaryCondition(VWT, north, 0.0);
+	fieldOper.boundaryCondition(VWT, south, 0.0);
+	fieldOper.boundaryCondition(VWT, east, 0.0);
+	fieldOper.boundaryCondition(VWT, west, 0.0);
 
-	fieldOper.boundaryCondition(WWC, north, 0.0);
-	fieldOper.boundaryCondition(WWC, south, 0.0);
-	fieldOper.boundaryCondition(WWC, east, 0.0);
-	fieldOper.boundaryCondition(WWC, west, 0.0);
-	fieldOper.boundaryCondition(WWC, bottom, 0.0);
+	fieldOper.boundaryCondition(WWT, north, 0.0);
+	fieldOper.boundaryCondition(WWT, south, 0.0);
+	fieldOper.boundaryCondition(WWT, east, 0.0);
+	fieldOper.boundaryCondition(WWT, west, 0.0);
 
-	fieldOper.boundaryCondition(PC, top, 0.0);
-	fieldOper.boundaryCondition(P, top, 0.0);
 
 	fieldOper.boundaryCondition(massFluxE, east, 0.0);
 	fieldOper.boundaryCondition(massFluxE, west, 0.0);
 	fieldOper.boundaryCondition(massFluxN, north, 0.0);
 	fieldOper.boundaryCondition(massFluxN, south, 0.0);
-	fieldOper.boundaryCondition(massFluxT, bottom, bottomwallvelformassfluxinit);
-	//fieldOper.print3dmat(U);
+	
+	//-----------------END-----------------------//
+
+
+
+
+	//------------BOTTOM OR INLET----------------//
+
+	fieldOper.boundaryCondition(U, bottom, 0.0);
+	fieldOper.boundaryCondition(V, bottom, 0.0);
+	fieldOper.boundaryCondition(W, bottom, inletwvel);
+
+	fieldOper.boundaryCondition(UT, bottom, 0.0);
+	fieldOper.boundaryCondition(VT, bottom, 0.0);
+	fieldOper.boundaryCondition(WT, bottom, inletwvel); 
+
+	fieldOper.boundaryCondition(UW, bottom, 0.0);
+	fieldOper.boundaryCondition(VW, bottom, 0.0);
+	fieldOper.boundaryCondition(WW, bottom, inletwvel);
+
+	fieldOper.boundaryCondition(UWT, bottom, 0.0);
+	fieldOper.boundaryCondition(VWT, bottom, 0.0);
+	fieldOper.boundaryCondition(WWT, bottom, inletwvel);
+
+	fieldOper.boundaryCondition(massFluxT, bottom, massfluxwinlet);
+
+	//------------------- END --------------------//
 
 
 
 
 
+	//--------------- TOP OR OUTLET --------------//
+
+	fieldOper.boundaryCondition(P, top, 0.0);
+	fieldOper.boundaryCondition(PT, top, 0.0);
+
+	//------------------- END --------------------//
 
 
 
 
 
+	//----------INITIALIZE INTERNAL FIELD---------//
+	
+	fieldOper.initializeInternalField(U, u_guess);
+	fieldOper.initializeInternalField(V, v_guess);
+	fieldOper.initializeInternalField(W, w_guess);
+	fieldOper.initializeInternalField(P, 0.0);
 
+	// initialize initial time step zero as this is not guess
 
+	fieldOper.initializeInternalField(UT, 0.0);
+	fieldOper.initializeInternalField(VT, 0.0);
+	fieldOper.initializeInternalField(WT, 0.0);
+	fieldOper.initializeInternalField(PT, 0.0);
 
+	fieldOper.initializeInternalField(UW, u_guess);
+	fieldOper.initializeInternalField(VW, v_guess);
+	fieldOper.initializeInternalField(WW, w_guess);
+
+	fieldOper.initializeInternalField(UWT, 0.0);
+	fieldOper.initializeInternalField(VWT, 0.0);
+	fieldOper.initializeInternalField(WWT, 0.0);
+
+	fieldOper.initializeInternalField(massFluxE, massfluxuguess);
+	fieldOper.initializeInternalField(massFluxN, massfluxvguess);
+	fieldOper.initializeInternalField(massFluxT, massfluxwguess);
+
+	//------------------END-----------------------//
 
